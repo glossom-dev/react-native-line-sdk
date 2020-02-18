148302bcecaed87129b845f63a58b7d28a9f59bf
kiyomasa-sato <kiyomasa.sato@gree.net>
2020-02-18 17:15:19 +0900

    example RN0.59.10

---
+++ example/metro.config.js
@@ -0,0 +1,24 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+const blacklist = require('metro-config/src/defaults/blacklist');
+
+// blacklist is a function that takes an array of regexes and combines
+// them with the default blacklist to return a single regex.
+
+module.exports = {
+  resolver: {
+    blacklistRE: blacklist([/\/node_modules\/react-native-line\/example\/.*/])
+  },
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+};
